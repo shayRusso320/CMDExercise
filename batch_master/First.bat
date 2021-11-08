1d56c4141e4882b7f6cccaa61fefeb9c9cccfd3b
shayRusso320 <shayrusso111@gmail.com>
2021-11-08 15:17:38 +0200

    removing dir first4

--- batch_master/First.bat
+++ batch_master/First.bat
@@ -4,9 +4,10 @@ set SCRIPTS=C:\code\Scripts
 mkdir %SCRIPTS%\First
 
 for /L %%i in (1, 1, 10) do (
-	mkdir %scripts%\First\folder%%i
+	mkdir %scripts%\First\first%%i
 )
 
+rmdir %scripts%\First\first4
 
 echo ALL DONE!
 PAUSE
